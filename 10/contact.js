6ba6f28559b8db441746aba6cac27d48846e41f7
Karina <karinakozarova1303@gmail.com>
2017-08-18 22:13:50 +0300

    external js #34

---
+++ 10/contact.js
@@ -0,0 +1,20 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+
+function initMap() {
+    var uluru = { lat: -25.363, lng: 131.044 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
